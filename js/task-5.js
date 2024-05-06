575b9b2f51d5023ff76dd7d5eca1b7c4c06f8b3e
Olexandrd <olexandrd@gmail.com>
2024-05-06 19:51:08 +0300

    Task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const span = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+	const color = getRandomHexColor();
+	body.style.backgroundColor = color;
+	span.textContent = color;
+});
