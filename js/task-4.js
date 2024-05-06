4259b4d4bc2e44e37b742803fb25151c9481ff6b
Olexandrd <olexandrd@gmail.com>
2024-05-06 19:34:41 +0300

    Task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const email = event.target.elements.email.value.trim();
+	const password = event.target.elements.password.value.trim();
+	const formObject = {
+		email,
+		password,
+	};
+	if (formObject.email === '' || formObject.password === '') {
+		alert('All form fields must be filled in');
+		return;
+	}
+	console.log(formObject);
+	event.currentTarget.reset();
+});
